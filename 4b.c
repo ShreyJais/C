bdb313ade7e0d5f733f119257cb60555085f3272
ShreyJais <78020396+ShreyJais@users.noreply.github.com>
2023-06-29 23:00:34 +0530

    Swap variable values of two variables
    //C program to swap variable values of two variables
    //Without Using a temporary variable

---
+++ 4b.c
@@ -0,0 +1,20 @@
+//C program to swap variable values of two variables
+//Without Using a temporary variable
+#include<stdio.h>
+int main() {
+    double a, b;
+    printf("Enter a: ");
+    scanf("%lf", &a);
+    printf("Enter b: ");
+    scanf("%lf", &b);
+
+    a = a - b;   
+ 
+    b = a + b;
+
+    a = b - a;
+
+    printf("After swapping, a = %.2lf\n", a);
+    printf("After swapping, b = %.2lf", b);
+    getch();
+}
