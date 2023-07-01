a041e7514f633cda4954dc0a531e2c14e5f2ebcb
ShreyJais <78020396+ShreyJais@users.noreply.github.com>
2023-07-01 10:48:56 +0530

    calculate x^y
    //C program to calculate x^y (for).

---
+++ 22.c
@@ -0,0 +1,21 @@
+//C program to calculate x^y (for).
+#include <stdio.h>
+int main()
+{
+	int base, exponent;
+	int result=1;
+	
+	printf("Enter a base number: ");
+	scanf("%d", &base);
+	printf("Enter an exponent: ");
+	scanf("%d", &exponent);
+
+	for(exponent; exponent>0; exponent--)
+	{
+		result=result*base;
+	}
+	printf("Result: %d", result);
+    
+    getch();
+	return 0;
+}
