ecfbab1f309e0de4fba7fc370ccb0ef8f89226a0
ShreyJais <78020396+ShreyJais@users.noreply.github.com>
2023-07-01 10:38:08 +0530

    sum of natural numbers between 1 to n
    /*C program to find sum of natural numbers between 1 to n*/

---
+++ 15.c
@@ -0,0 +1,22 @@
+/*C program to find sum of natural numbers between 1 to n*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum=0;
+
+    /* Input upper limit from user */
+    printf("Enter upper limit: ");
+    scanf("%d", &n);
+
+    /* Find sum of all numbers */
+    for(i=1; i<=n; i++)
+    {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers = %d", n, sum);
+    getch();
+    return 0;
+}
